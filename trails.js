b342fb9ffe99aacb415dcbc505726a9aac7a453b
kimberleyworld <83449648+kimberleyworld@users.noreply.github.com>
2025-04-02 17:37:32 +0100

    Adding hearts and sanitised

--- trails.js
+++ trails.js
@@ -1,18 +1,19 @@
 let trails = [];
 
 function setup() {
-  const canvas = createCanvas(windowWidth, windowHeight);
+  const canvas = createCanvas(windowWidth, document.body.scrollHeight);
   canvas.style("position", "absolute"); // Position the canvas absolutely
   canvas.style("top", "0");
   canvas.style("left", "0");
-  canvas.style("z-index", "-1"); // Send the canvas behind other elements
+  canvas.style("z-index", "-100"); // Send the canvas behind other elements
   canvas.style("pointer-events", "none"); // Allow clicks to pass through the canvas
   noCursor();
   noFill();
 }
 
 function draw() {
-  background(255, 255, 255, 20); // White background with low opacity for fading effect
+    background(100, 149, 237, 20); // Cornflower blue background with low opacity for fading effect
+
   heart(mouseX, mouseY, 15);
 }
 
